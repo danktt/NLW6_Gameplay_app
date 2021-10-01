b2758b2fb7bfe3d492d88e86033f9485a84253ef
danktt <danilomirandaa.15@outlook.com>
2021-09-30 23:12:13 -0400

    ButtonAdd-finish

---
+++ src/components/Category/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    
+  },
+  content: {
+
+  },
+  checked: {
+
+  },
+  check: {
+
+  },
+  title:{
+    
+  },
+
+})
--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -2,18 +2,35 @@ import React from 'react';
 
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { categories } from '../../utils/categories';
+import Category from '../Category';
 import { styles } from './styles';
 
+type Props = {
+  categorySelected : string,
+}
 
-
-const CategorySelect: React.FC = () => {
+const CategorySelect: React.FC<Props> = ({categorySelected}) => {
   return(
     <ScrollView
      style={styles.container}
      horizontal
      showsHorizontalScrollIndicator={false}
      contentContainerStyle={{paddingRight: 40}} 
-    />
+    >
+
+      {
+        categories.map(category =>(
+          <Category
+            key={category.id}
+            title={category.title}
+            icon={category.icon}
+            checked={category.id === categorySelected} 
+
+          />
+        ))
+      }
+    </ScrollView>
 
 
 
